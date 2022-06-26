5612df42bcc9f9a9436e9364042c6b7b0f0caea4
Matthias Schneider <schneidermjs1@gmail.com>
2022-06-26 22:44:16 +0200

    adjustments for new use case

--- main.py
+++ main.py
@@ -4,33 +4,14 @@ from datetime import date
 from news_extractor import News_Extractor
 from constant import Topic, DATEFORMAT
 
-@click.command()
-@click.option("-s", "--start_date", type=str, help="The earliest publication date of an article (format YYYY-MM-DD)")
-@click.option("-e", "--end_date", type=str, help="The last publication date of an article (format YYYY-MM-DD). If this option is not provided, it defaults to the current date")
-def run(start_date: str, end_date: str):
-    if(start_date is None):
-        print("No valid start date provided")
-        exit(1)
-    
-    try:
-        s_date = datetime.datetime.strptime(start_date, DATEFORMAT).date()
-    except:
-        print("No valid start date provided. Please use the following format: YYYY-MM-DD")
-        exit(1)
-    
-    if(end_date is None):
-        e_date = date.today()
-    else:
-        try:
-            e_date = datetime.datetime.strptime(end_date, DATEFORMAT).date()
-        except:
-            print("No valid end date provided. Please use the following format: YYYY-MM-DD")
-            exit(1)
+
+def run():
 
     news_crawler = News_Extractor()
-    print("News crawling started (" + s_date.strftime(DATEFORMAT) + " - " + e_date.strftime(DATEFORMAT) + ")")
-    
-    news_crawler.crawl(s_date, e_date)
+    print("News crawling started...")
+
+    news_crawler.crawl()
+
 
 if __name__ == "__main__":
     run()
--- news_extractor.py
+++ news_extractor.py
@@ -2,61 +2,86 @@ import logging
 from time import sleep
 from datetime import date, datetime, timedelta
 
-#import elementpath
+# import elementpath
 import lxml.etree as etree
 from lxml.etree import fromstring
 
 import requests
-from parsel import Selector
 
 from news_producer import News_Producer
 from build.gen.article_pb2 import Article
 
+import pandas as pd
+
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import scan
+
+import urllib.parse
+
 from constant import DATEFORMAT
 
-
 log = logging.getLogger(__name__)
+
+es = Elasticsearch(['localhost:9200'])
+
+es.info()
+
 
 class News_Extractor:
     def __init__(self):
         self.news_producer = News_Producer()
-        self.keywords = [
-            "Wirtschaft", 
-            "Politik", 
-            "Firma", 
-            "Umsatz", 
-            "Gewinn", 
-            "Neueintragung", 
-            "Skandal",
-            "Management",
-            "Konzern",
-            "Unternehmen",
-            "Covid",
-            "Betrieb",
-            "Aktionäre",
-            "Ukraine",
-            "Russland",
-            "Öl",
-            "Preise"]
+        self.keywords = []
 
-    def crawl(self, start_date: date, end_date: date):
-        delta = timedelta(days=1)
+    def get_data_from_elastic(self, table_name):
+        query = {
+            "query": {
+                "match_all": {}
+            }
+        }
 
-        while start_date <= end_date:
-            for keyword in self.keywords:
-                end_date_temp = start_date + delta
+        rel = scan(client=es,
+                   query=query,
+                   scroll='1m',
+                   index=table_name,
+                   raise_on_error=True,
+                   preserve_order=False,
+                   clear_scroll=True)
 
-                print("crawling keyword " + keyword + " within daterange: " + start_date.strftime(DATEFORMAT) + " - " + end_date_temp.strftime(DATEFORMAT) )
-                
-                self.crawl_by_keyword_and_date(keyword, start_date, end_date_temp)
-                sleep(0.5)
+        result = list(rel)
+        temp = []
 
-            start_date += delta
+        for hit in result:
+            temp.append(hit['_source'])
 
-    def crawl_by_keyword_and_date(self, keyword, start_date, end_date):
+        df = pd.DataFrame(temp)
+        return df
+
+    def crawl(self):
+        df_rb = self.get_data_from_elastic("corporate-events-full")
+
+        for i in range(0, 20000):
+            rb_id = df_rb["id"][i]
+            info = df_rb["information"][i]
+
+            keyword = self.find_company(info)
+            rb_id = rb_id
+
+            self.crawl_by_keyword_and_date(keyword, rb_id)
+
+    def find_company(self, inp):
+        start = inp.find(":")
+        end = inp.find(",")
+        company = inp[start + 2:end]
+
+        if (len(company) == 0):
+            company = inp[0:inp.find(",")]
+
+        return company
+
+    def crawl_by_keyword_and_date(self, keyword, rb_id):
         try:
-            text, url = self.send_request(keyword, start_date, end_date)
-            
+            text, url = self.send_request(keyword)
+
             xml = text.encode('utf-8')
             parser = etree.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
             root = fromstring(xml, parser=parser)
@@ -79,24 +104,24 @@ class News_Extractor:
 
                         if elem.tag == "link":
                             article.link = elem.text
-                        
+
                         if elem.tag == "guid":
                             article.id = elem.text
 
                         if elem.tag == "pubDate":
                             article.publication_date = elem.text
-                        
+
                         if elem.tag == "description":
                             article.description = elem.text
-                        
+
                         if elem.tag == "source":
                             article.source = elem.text
 
-                    article.search_keyword = keyword
+                    article.search_keyword = keyword + "%" + rb_id
                     article.search_url = url
                     # print(article.title + " " + keyword + " " + url)
                     # print("-------------------------------------------------------")
-                    
+
                     self.news_producer.produce_to_topic(article)
 
 
@@ -104,8 +129,10 @@ class News_Extractor:
             print(ex)
             log.error(f"Error")
 
-    def send_request(self, keyword, start_date, end_date) -> str:
-        url = f"https://news.google.com/rss/search?q=" + keyword + "+after:" + start_date.strftime(DATEFORMAT) + "+before:" + end_date.strftime(DATEFORMAT) + "&ceid=DE:de&hl=de&gl=DE"
+    def send_request(self, keyword) -> str:
+        keyword = urllib.parse.quote(keyword)
+
+        url = f"https://news.google.com/rss/search?q=" + keyword + "&ceid=DE:de&hl=de&gl=DE"
 
         print("used url: " + url)
 
